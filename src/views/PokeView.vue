7ef140bd256f631da0c0f3e5be9e74924afb0da9
maydelpz <maydelinlopez454@gmail.com>
2024-09-09 09:38:39 -0600

    pinia listo

--- src/views/PokeView.vue
+++ src/views/PokeView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { useRoute, useRouter } from 'vue-router';
 import {useGetData} from '@/composables/getData';
+import {useFavoritosStore} from '@/store/favoritos';
 
 const router = useRouter();
 const route = useRoute();
+const useFavoritos = useFavoritosStore();
+
+const { add, findPoke } = useFavoritos;
 
 const {data, getData, loading, errorData} = useGetData();
 const back = () => {
@@ -19,6 +23,7 @@ getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
     <div v-if="data">
         <img :src="data.sprites?.front_default" alt="">
         <h1>Poke name: {{ $route.params.name }}</h1>
+        <button :disabled="findPoke(data.name)" class="btn btn-primary m-2" @click="add(data)">Agregar a favoritos</button>
     </div>
     <button @click="back" class="btn btn-outline-primary m-2">Volver</button>
 </template>
